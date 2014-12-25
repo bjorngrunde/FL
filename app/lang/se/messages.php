f141387b20073639817b442f479a3731da492899
Björn <bjorngrunde@live.se>
2014-12-25 05:41:04 +0000

    romved stuff

---
+++ app/lang/se/messages.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+
+	'comments_heading' => 'Kommentarer',
+	'no_comments' => 'Det finns inga kommentarer än, var den första att kommentera.',
+	'add_comment_heading' => 'Skapa en kommentar',
+	'add_form_anchor' => 'add-comment',
+	'add_form_link_text' => 'Skapa en kommentar',
+	'login_required' => 'Du måste vara inloggar för att kunna kommentera.',
+	'login_btn_text' => 'Logga in',
+	'register_btn_text' => 'Registrera',
+
+	'form' => array(
+		'label' => 'Kommentar',
+		'placeholder' => 'Lägg till dina kommentarer här...',
+		'submit' => 'Lägg till kommentar',
+	),
+
+	'unexpected_error' => 'Ett oväntat fel uppstod och din kommentar kan inte läggas till, detta är förmodligen Swings fel.',
+
+	'date_format' => 'Den %e %B %Y  klockan: %H:%M',
+	'date_locale' => 'se',
+	'date_timezone' => 'Europe/Stockholm'
+
+);
