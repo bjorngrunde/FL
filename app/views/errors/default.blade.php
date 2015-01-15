179996230aaa53a8a517e705662cb1d27d561561
Björn Grunde <bjorngrunde@live.se>
2015-01-15 21:14:09 +0100

    FFixade bug och lade till server error pages

--- app/views/errors/default.blade.php
+++ app/views/errors/default.blade.php
@@ -4,11 +4,12 @@
 
     <div class="row">
     <div class="col-md-12 text-center">
-        <h1>Attans..</h1>
-        <div style="width:100%; height: 300px; background: url('/img/gvg.jpg')no-repeat center center; -webkit-background-size: cover; -moz-background-size: cover; background-size: cover;">
 
-        </div>
+        <div class="dark-sh-well" style="width:100%; height: 300px; background: url('/img/gvg.jpg')no-repeat center center; -webkit-background-size: cover; -moz-background-size: cover; background-size: cover;">
+        <h1>Attans..</h1>
         <h5>Vi har ingen aning vad som hänt, försök gå tillbaka till<a href="/"> Startsidan!</a> </h5>
+        </div>
+
 
     </div>
     </div>
