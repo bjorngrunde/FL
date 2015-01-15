9e4d3370370ea9145ce4dfe3df8b0a3b235b316e
Björn Grunde <bjorngrunde@live.se>
2015-01-15 19:36:56 +0100

    Byggde simpel sökfunktion

---
+++ app/controllers/SearchController.php
@@ -0,0 +1,33 @@
+<?php
+
+class SearchController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function query()
+    {
+        $query = Input::get('term');
+        $users = User::where('username', 'LIKE', "%$query%")->get();
+        $result = [];
+        foreach($users as $user)
+        {
+            $result[] = $user->username;
+        }
+        return Response::json($result);
+    }
+
+    public function searchResult()
+    {
+        $result = Input::get('auto');
+
+        if($user = User::whereUsername($result)->firstOrFail())
+        {
+            return Redirect::to('/profile/'.$user->username);
+        }
+
+    }
+
+}
