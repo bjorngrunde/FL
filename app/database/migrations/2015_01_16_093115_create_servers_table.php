7ab1110c754f27b008f568338c761e42dd2049e0
Björn Grunde <bjorngrunde@live.se>
2015-01-16 10:59:53 +0100

    Merge branch 'master' of https://github.com/bjorngrunde/FL
    Conflicts:
    	.idea/workspace.xml
    	app/controllers/RaidsController.php

---
+++ app/database/migrations/2015_01_16_093115_create_servers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('servers', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('server');
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('servers');
+	}
+
+}
