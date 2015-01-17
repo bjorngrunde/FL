fc6dfbca0bf550975085fd61b9e6b4b26d0a8f06
Björn Grunde <bjorngrunde@live.se>
2015-01-17 01:39:35 +0100

    domain design

--- app/models/Application.php
+++ app/models/Application.php
@@ -1,10 +1,12 @@
 <?php
 
-
+use Family\Eventing\EventGenerator;
 
 class Application extends Eloquent
 {
-    protected $table = 'applications';
+    use EventGenerator;
+
+    protected $table = 'Applications';
 
     protected $fillable = [
         'name',
@@ -14,7 +16,6 @@ class Application extends Eloquent
         'server',
         'talents',
         'armory',
-        'choices',
         'played',
         'playClass',
         'bio',
@@ -28,11 +29,58 @@ class Application extends Eloquent
         'klass'
     ];
 
-    protected $guarded = ['id'];
+    protected $guarded = [];
 
     /**
      * @return mixed
      */
+    public function post($name, $lastName, $username, $email, $server, $talents, $armory, $played, $playClass, $bio, $raidExperience, $reasonToApplyFl, $oldGuild, $progressRaid, $attendance, $screenshot, $klass)
+    {
+        $this->name             = $name;
+        $this->lastName         = $lastName;
+        $this->username         = $username;
+        $this->email            = $email;
+        $this->server           = $server;
+        $this->talents          = $talents;
+        $this->armory           = $armory;
+        $this->played           = $played;
+        $this->playClass        = $playClass;
+        $this->bio              = $bio;
+        $this->raidExperience   = $raidExperience;
+        $this->reasonToApplyFl  = $reasonToApplyFl;
+        $this->oldGuild         = $oldGuild;
+        $this->progressRaid     = $progressRaid;
+        $this->attendance       = $attendance;
+        $this->klass            = $klass;
+        if(Input::hasfile($screenshot))
+        {
+            try
+            {
+                $file = Input::file($screenshot);
+                $filename = time() . '-application.jpg';
+                $filepath = '/img/applications/';
+                $file = $file->move(public_path($filepath),$filename);
+
+                $this->screenshot = $filepath.$filename;
+            }
+            catch(Exception $e)
+            {
+                return 'Något gick snett mannen: ' .$e;
+            }
+        }
+        $this->save();
+
+        $status = new Status;
+        $status->app_status = 'default';
+        $status->save();
+
+        $status->Applications()->save($this);
+
+        $this->raise(new ApplicationWasPosted($this));
+
+        return $this;
+    }
+
     public function status()
     {
         return $this->belongsTo('Status');
