a2c898b756d8c1ac08f3fb2829a3ea0415be3bb0
Björn Grunde <bjorngrunde@live.se>
2015-01-18 16:58:57 +0100

    domain design

--- app/Family/Commanding/CommandBus.php
+++ app/Family/Commanding/CommandBus.php
@@ -1,27 +1,7 @@
 <?php
-
 namespace Family\Commanding;
 
-
-use Illuminate\Foundation\Application;
-
-class CommandBus {
-
-    protected $commandTranslator;
-    /**
-     * @var Application
-     */
-    private $application;
-
-    function __construct(Application $application, CommandTranslator $commandTranslator)
-    {
-        $this->application = $application;
-        $this->commandTranslator = $commandTranslator;
-    }
-    public function execute($command)
-    {
-        $handler = $this->commandTranslator->toCommandHandler($command);
-
-        return $this->application->make($handler)->handle($command);
-    }
-} 
+interface CommandBus
+{
+    public function execute($command);
+}
---
+++ app/Family/Commanding/DefaultCommandBus.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Family\Commanding;
+
+use Illuminate\Foundation\Application;
+
+class DefaultCommandBus implements CommandBus
+{
+    protected $commandTranslator;
+    private $application;
+
+    function __construct(Application $application, CommandTranslator $commandTranslator)
+    {
+        $this->application = $application;
+        $this->commandTranslator = $commandTranslator;
+    }
+    public function execute($command)
+    {
+        $handler = $this->commandTranslator->toCommandHandler($command);
+
+        return $this->application->make($handler)->handle($command);
+    }
+} 
