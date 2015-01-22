0dd37c63a99433983ba6b0fcdda6bcd944c962a8
Björn Grunde <bjorngrunde@live.se>
2015-01-22 17:34:42 +0100

    Image gallery

---
+++ app/views/packages/jeroen-g/laravel-photo-gallery/new.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.gallery')
+@section('gallerySection')
+
+<div class="row">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <b>{{ Lang::get('gallery::gallery.create') . ' ' . Lang::choice("gallery::gallery.$type", 1) }}</b>
+        </div>
+        <div class="panel-body">
+
+    {{ $form }}
+
+    @if ($errors->any())
+            <ul>
+                {{ implode('', $errors->all('<li class="error">:message</li>')) }}
+            </ul>
+    @endif
+        </div>
+    </div>
+</div>
+
+@stop
+
+
---
+++ app/views/packages/jeroen-g/laravel-photo-gallery/photo.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.gallery')
+@section('gallerySection')
+
+<div class="row">
+    <div class="panel panel-default">
+        <div class="panel-heading clearfix">
+            <b>{{ $photo->photo_name }}</b>
+            <div class="pull-right">
+                <a class="btn btn-default" href="{{ URL::route('gallery.album.show', array('id' => $photo->album_id)) }}">{{ Lang::get('gallery::gallery.return') }}</a>
+            </div>
+        </div>
+        <div class="panel-body">
+            <img class="img" src='{{ asset("uploads/photos/" . $photo->photo_path ) }}' />
+        </div>
+        <div class="panel-footer clearfix">
+    	    {{ $photo->photo_description }}
+            {{ Form::open(array('route' => array("gallery.album.photo.destroy", $photo->album_id, $photo->photo_id))) }}
+                    {{ link_to_route("gallery.album.photo.edit", Lang::get('gallery::gallery.edit'), array('albumId' => $photo->album_id, 'photoId' => $photo->photo_id), array('class' => 'btn btn-info')) }}
+                    {{ Form::hidden('_method', 'DELETE') }}
+                    {{ Form::submit(Lang::get('gallery::gallery.delete'), array('class' => 'btn btn-danger')) }}
+                {{ Form::close() }}
+        </div>
+    </div>
+</div>
+	
+@stop
