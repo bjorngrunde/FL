2dac2b66b3eed7954904a39ee46eebfd4ae34ec5
Björn Grunde <bjorngrunde@live.se>
2015-03-01 13:19:52 +0100

    fixes

--- public/js/notifications/notiser.js
+++ public/js/notifications/notiser.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var i = 0;
-    $.get('http://family.dev:8000/notifications', function(data){
+    $.get('http://178.62.115.229/notifications', function(data){
         $.each(data, function(index, value) {
             $('#notificationMenu').append('<li><a href="#" class="dark-sh-well-no-radius">' + value.body +'</a></li>');
             if(value.is_read == 0)
