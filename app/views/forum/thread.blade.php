fc431d016eb8302f4050e7bab66a42a4d931e7b8
Björn Grunde <bjorngrunde@live.se>
2015-03-03 08:39:28 +0100

    fixes

--- app/views/forum/thread.blade.php
+++ app/views/forum/thread.blade.php
@@ -88,9 +88,9 @@
             <div class="col-sm-12 dark-sh-well-no-radius">
             <div class="col-sm-12">
              @if(Auth::user()->hasRole('Admin') || Auth::user()->hasRole('Utvecklare') || Auth::user()->username == $comment->author->username)
-             <ul class="list-inline list-unstyled">
-                <li><a href="#" class="pull-right" data-toggle="modal" data-target="#comment-edit"><span id="{{$comment->id}}" class="fa fa-pencil delete_comment"></span></a></li>
-                <li><a href="#" class="pull-right" data-toggle="modal" data-target="#comment-delete"><span id="{{$comment->id}}" class="fa fa-times delete_comment"></span></a></li>
+             <ul class="list-inline list-unstyled pull-right">
+                <li><a href="#"  data-toggle="modal" data-target="#comment-edit"><span id="{{$comment->id}}" class="fa fa-pencil delete_comment"></span></a></li>
+                <li><a href="#"  data-toggle="modal" data-target="#comment-delete"><span id="{{$comment->id}}" class="fa fa-times delete_comment"></span></a></li>
             </ul>
             @endif
             </div>
