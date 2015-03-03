03e85ba599c910da516aaa5c40499d64ff4690a7
Björn Grunde <bjorngrunde@live.se>
2015-03-03 11:28:16 +0100

    fixes

--- app/views/packages/jeroen-g/laravel-photo-gallery/album.blade.php
+++ app/views/packages/jeroen-g/laravel-photo-gallery/album.blade.php
@@ -18,7 +18,7 @@
             @foreach($albumPhotos as $photo)
                 <div class="col-sm-4 col-md-4 col-lg-4">
                     <a href="{{ asset('uploads/photos/' .$photo->thumbnail)}}" data-lightbox-gallery="{{$album->album_name}}" data-lightbox-title="{{$photo->photo_name}}" class="lightbox">
-                    <img src="{{asset('uploads/thumbnails/'. $photo->thumbnail)}}" class="img-responsive"/> </a>
+                    <img src="{{asset('uploads/thumbnails/'. $photo->thumbnail)}}" class="img-responsive dark-sh-well-no-radius-img"/> </a>
                     <a href="/gallery/album/{{$album->album_id}}/photo/{{$photo->photo_id}}"><h5 class="text-center">{{$photo->photo_name}}</h5></a></b>
                     <small class="text-center">{{$photo->photo_description}}</small>
                 </div>
